78320cd56d69fbdbce5adcddcc76b396f12f11a3
anothercommit <jdomenech@escuelasproa.edu.ar>
2022-08-29 14:20:17 -0300

    pasar cosas del front

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const port = 3900;
+const port = 3700;
 //Corremos el servidor en el puerto seleccionado
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port} correctamente`);
