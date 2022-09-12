b6a125731ccf2e5618f794ba97190cb3cc29db76
anothercommit <jdomenech@escuelasproa.edu.ar>
2022-09-12 12:30:38 -0300

    emi gay y jere tambien

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
 const session = require("cookie-session");
-
+const nodemailer = require("nodemailer");
 //Base de Datos
 const mongoose = require("mongoose");
 const Admin = require("./models/myModel");
@@ -250,6 +250,38 @@ app.post("/ChangeDatos", (req, res) => {
 app.get("/*", (req, res) => {
     res.status(200).render("error404");
     
+});
+
+
+app.post("/contactForm", async (req, res) => {
+    //Envio de mail de contacto
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "jaguerodiaz@escuelasproa.edu.ar",
+            pass: "jere172901",
+        },
+    });
+
+    // send mail with defined transport object
+    let contenido = {
+        from: "jaguerodiaz@escuelaproa.edu.ar", // sender address
+        to: "jjgenio.com@gmail.com", // list of receivers
+        subject: req.body.nombre + req.body.apellido, // Subject line
+        text:
+            "consulta:" +
+            req.body.consulta +
+            "mail del paciente:" +
+            req.body.correo, // plain text body
+    };
+    transporter.sendMail(contenido, function (err, data) {
+        if (err) {
+            console.log(`error encontrado : ${err}`);
+        } else {
+            console.log(`Email enviado`);
+        }
+    });
+    res.render("index");
 });
 
 
